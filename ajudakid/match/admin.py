2756defb49e8bea8e6fc121e1ca9deabb985ee93
shibiribiu <eduardo.rborges2002@gmail.com>
2019-10-19 02:48:42 -0300

    models addded to admin

--- ajudakid/match/admin.py
+++ ajudakid/match/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from match.models import AcaoApoiador, Acao, Apoiador, Badges, Entidade
 
 # Register your models here.
+admin.site.register(Apoiador)
+admin.site.register(Entidade)
+admin.site.register(Badges)
+admin.site.register(Acao)
+admin.site.register(AcaoApoiador)
