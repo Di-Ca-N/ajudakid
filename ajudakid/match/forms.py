f162a9184ca2f08b0d729b467baa545a54686b63
shibiribiu <eduardo.rborges2002@gmail.com>
2019-10-19 03:40:07 -0300

    Modified forms

--- ajudakid/match/forms.py
+++ ajudakid/match/forms.py
@@ -5,15 +5,11 @@ from django.contrib.auth.models import User
 
 
 class EntidadeForm(forms.ModelForm):
-	username = forms.CharField(widget=forms.TextInput(attrs={'class':''}))
-	endereco = forms.CharField()
-	cnpj = forms.CharField()
-
 	class Meta:
 		model = Entidade
 		widgets = {
 			'nome': forms.TextInput(attrs={'class':''}),
-			'username': ,
+			'username': forms.TextInput(attrs={'class':''}),
 			'email': forms.EmailInput(attrs={'class':''}),
 			'endereco': forms.TextInput(attrs={'class':''}),
 			'cnpj': forms.TextInput(attrs={'class':''}),
@@ -21,24 +17,6 @@ class EntidadeForm(forms.ModelForm):
 			'confirmacao_senha': forms.PasswordInput(attrs={'class':''}),
 		}
 
-	def clean(self):
-		super().clean()
-		username = self.cleaned_data['username']
-		if username in User.objects.values_list('username', flat=True):
-			self.add_error('username', error_message='Esse nome de usuário já existe.')
-
-	def save(self, commit=True):
-		username = self.cleaned_data['username']
-		user = User.objects.create_user(username)
-
-		nome = self.cleaned_data['nome']
-		email = self.cleaned_data['email']
-		endereco = self.cleaned_data['endereco']
-		cnpj = self.cleaned_data['cnpj']
-		senha = self.cleaned_data['senha']
-		confirmacao_senha = self.cleaned_data['confirmacao_senha']
-
-		Entidade.objects.create(owner=user, nome=nome, endereco=endereco, cnpj=cnpj)
 
 class ApoiadorForm(ModelForm):
 	class Meta:
@@ -54,24 +32,3 @@ class ApoiadorForm(ModelForm):
 			'registro': forms.TextInput(attrs={'class':''}),
 			'tipo': forms.ChoiceWidget(attrs={'class':''}),
 		}
-
-	def clean(self):
-		super().clean()
-		username = self.cleaned_data['username']
-		if username in User.objects.values_list('username', flat=True):
-			self.add_error('username', error_message='Esse nome de usuário já existe.')
-
-	def save(self, commit=True):
-		username = self.cleaned_data['username']
-		user = User.objects.create_user(username)
-
-		nome = self.cleaned_data['nome']
-		email = self.cleaned_data['email']
-		endereco = self.cleaned_data['endereco']
-		cnpj = self.cleaned_data['cnpj']
-		senha = self.cleaned_data['senha']
-		confirmacao_senha = self.cleaned_data['confirmacao_senha']
-		registro = 
-		tipo = self.cleaned_data[]
-
-		Apoiador.objects.create(owner=user, nome=nome, email=email)
