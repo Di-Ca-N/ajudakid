cda5ea640843244ffa3760afaedbb83504732c07
shibiribiu <eduardo.rborges2002@gmail.com>
2019-10-19 05:40:18 -0300

    vai caceta

--- ajudakid/match/views.py
+++ ajudakid/match/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login
 from .forms import EntidadeForm, ApoiadorForm, EnderecoForm
 
 def quem_somos(request):
-	return HttpResponse("Olá")
+	return render(request, 'match/quem_somos.html', context)
 
 def entidades(request):
 	return HttpResponse("Olá")
@@ -71,51 +71,3 @@ def sucesso_cadastro(request):
 
 def cadastrar_acao(request):
 	return HttpResponse("None")
-''' from django.shortcuts import render
-from models import Entidade
-from models import Apoiadores
-from .forms import Formulario
-
-def home(request):
-	context = {}
-	return render(request, 'home/index.html', context)
-
-def quem_somos(request):
-	context = {}
-	return render(request, 'quem_somos/index.html', context)
-
-def entidades(request):
-	context = {'entidades':Entidade.objects.all()}
-	return render(request, 'entidades/index.html', context)
-
-def ranking(request):
-	context = {'classificados': Apoiadores.objects.all()}
-	return render(request, 'ranking/index.html', context)
-
-def cadastro_apoiador(request):
-	if request.method == "POST":
-		form = Formulario(request.POST)
-		
-		if form.is_valid():
-			user = form.save()
-			#------------VERIFICAR----------------
-
-			return render(request, 'login/login.html', )
-	else:
-		context = {'form': Formaulario()}
-		return render(request, 'cadastro_apoiador/index.html', context)
-
-def cadastro_entidade(request):
-	if request.method == "POST":
-		form = Formulario(request.POST)
-		
-		if form.is_valid():
-			user = form.save()
-			#------------VERIFICAR----------------
-
-			return render(request, 'login/login.html', )
-	else:
-		context = {'form': Formaulario()}
-		return render(request, 'cadastro_aentidade/index.html', context)
-
-'''
